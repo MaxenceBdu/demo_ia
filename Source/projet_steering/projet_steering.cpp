bd83ce6e1cfb0ceecd446a690137aaa198b5207f
Boisedu Maxence <maxence_boisedu@ens.univ-artois.fr>
2023-01-13 11:46:53 +0100

    Initial commit

---
+++ Source/projet_steering/projet_steering.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "projet_steering.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, projet_steering, "projet_steering" );
---
+++ Source/projet_steering/projet_steeringGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "projet_steeringGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJET_STEERING_API Aprojet_steeringGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
